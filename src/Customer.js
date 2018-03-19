a35ae44539b98842c0ce2c449671bedc56292c53
Xavier Er <xavier_er@live.com.sg>
2018-03-19 15:42:43 +0800

    Update

---
+++ src/Customer.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+import CustomerDetails from './CustomerDetails';
+
+import css from './Customer.css';
+
+class Customer extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="customer-id">{this.props.info.id}</div>
+        <CustomerDetails accounts={this.props.info.attributes.accounts}/>
+      </div>
+    );
+  }
+}
+
+export default Customer;
--- src/List.js
+++ src/List.js
@@ -1,36 +1,70 @@
 import React, { Component } from 'react';
 
-import Button from './Button';
-import ListItem from './ListItem';
+import Customer from './Customer';
 
 class List extends Component {
   constructor(props) {
     super(props);
-    this.state = { items: [] };
+    this.state = { customers: [] };
   }
 
   componentDidMount() {
     this.setState({
-      items: [
-        { name: 'Item 1' },
-        { name: 'Item 2' },
-        { name: 'Item 3' },
-        { name: 'Item 4' },
+      customers: [
+        {
+          "id": "C000001",
+          "type": "customers",
+          "attributes": {
+            "customer-relationship-id": "QAZWSX001RT",
+            "customer-name": "Michael",
+            "accounts": [
+              {
+                "account-id": "A000001"
+              },
+              {
+                "account-id": "A000002"
+              }
+            ]
+          },
+          "links": {
+            "self": "http://127.0.0.1:8080/retail/api/v3/customers/C000001"
+          }
+        },
+        {
+          "id": "C000002",
+          "type": "customers",
+          "attributes": {
+            "customer-relationship-id": "QAZWSX002RT",
+            "customer-name": "Queenie",
+            "accounts": [
+              {
+                "account-id": "A000003"
+              },
+              {
+                "account-id": "A000004"
+              },
+              {
+                "account-id": "A000005"
+              },
+              {
+                "account-id": "A000006"
+              },
+            ]
+          },
+          "links": {
+            "self": "http://127.0.0.1:8080/retail/api/v3/customers/C000002"
+          }
+        }
       ]
     });
-  }
-
-  handleButtonClick() {
-    alert('click');
   }
 
   render() {
     return (
       <div>
-        <Button onButtonClick={this.handleButtonClick} />
         {
-          this.state.items.map((item, i) => {
-            return <ListItem item={item} />;
+          this.state.customers.map((item, i) => {
+            return <Customer info={item} />;
           })
         }
       </div>
